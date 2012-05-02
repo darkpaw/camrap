f586a98caef162a9d1eac6daebba81f1f5f5b58e
Felix Sheldon <darkpaw@internode.on.net>
2012-05-02 20:15:19 +1000

    GPL notice

--- tools/patterngen.py
+++ tools/patterngen.py
@@ -1,3 +1,24 @@
+# Copyright 2012 Felix Sheldon
+#
+# This file is part of Camrap.
+#
+# Camrap is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by 
+# the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version.
+#
+# Camrap is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# 
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Camrap. If not, see http://www.gnu.org/licenses/.
+#
+
+
+
 import random
 import string
 
@@ -67,7 +88,7 @@ while dupe_count > 0:
 	print "#, # dupes", count, dupe_count		
 	
 	
-f = open("trinary,,x...txt", 'wt')
+f = open("trinary_out.txt", 'wt')
 for y, line in enumerate(lines):
 	print string.join(line, '') 
 	f.write(string.join(line, '') + '\n')
